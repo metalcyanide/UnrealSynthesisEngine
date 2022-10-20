96aa8c0371b83e14b23a928fb4dea764a65982cb
Shoooooon <san7@rice.edu>
2022-10-19 19:44:05 -0500

    Added example Java class.

---
+++ src/ExampleClass.java
@@ -0,0 +1,12 @@
+public class ExampleClass {
+  public ExampleClass()
+  {
+    String value = "Test";// Local Variable inside a Constructor
+  }
+
+  public void disp()
+  {
+    String name = "Stolen from web";// Local variable inside method
+    System.out.println("Welcome " + name + "!!!");
+  }
+}
