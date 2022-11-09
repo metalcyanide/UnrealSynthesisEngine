39a181ef9220b33130acf8a1710e558772a384f6
Shoooooon <san7@rice.edu>
2022-11-09 15:20:38 -0600

    Added dummy factories for all proof rules.

--- src/Proofs/ProofFactory.java
+++ src/Proofs/ProofFactory.java
@@ -4,7 +4,27 @@ import Proofs.Claim.IClaim;
 import java.util.function.Function;
 
 public enum ProofFactory {
-  WEAKEN((x) -> true);
+  Assign((x) -> true),
+  Seq((x) -> true),
+  Zero((x) -> true),
+  One((x) -> true),
+  True((x) -> true),
+  False((x) -> true),
+  Var((x) -> true),
+  Plus((x) -> true),
+  GrmDisj((x) -> true),
+  Not((x) -> true),
+  Comp((x) -> true),
+  Inv((x) -> true),
+  HP((x) -> true),
+  ApplyHP((x) -> true),
+  And((x) -> true),
+  ITE((x) -> true),
+  While((x) -> true),
+  Weaken((x) -> true),
+  Conj((x) -> true),
+  Sub1((x) -> true),
+  Sub2((x) -> true);
 
 
   private Function<Proof, Boolean> validationFunction;
