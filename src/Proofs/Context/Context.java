bab1e0b3679313c60b652f3fb3873bb1c9e29fc6
Shoooooon <san7@rice.edu>
2022-12-13 17:39:31 -0600

    I broke everythinggit add *!

--- src/Proofs/Context/Context.java
+++ src/Proofs/Context/Context.java
@@ -1,5 +1,7 @@
 package Proofs.Context;
 
+import ConditionLanguage.Condition;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import ConditionLanguage.ICondition;
@@ -9,14 +11,24 @@ import GrammarsLanguage.IProgram;
  * Dummy stand-in for context class. Should have some functionality... eventually.
  */
 public class Context implements IContext {
-  private String context;
+  private ArrayList<ICondition> preconds;
+  private ArrayList<IProgram> progs;
+  private ArrayList<ICondition> postconds;
 
-  public Context(String context) {
-    this.context = context;
+//  public Context(String context) {
+//    String[] triples = context.split(",");
+//  }
+
+  private Context(Context cont) {
+    this.preconds = cont.getPreconds();
+    this.progs = cont.getProgs();
+    this.postconds = cont.getPostconds();
   }
 
   public Context(Scanner readFrom) throws Exception {
-    StringBuilder contString = new StringBuilder();
+    preconds = new ArrayList<ICondition>();
+    progs = new ArrayList<IProgram>();
+    postconds = new ArrayList<ICondition>();
 
     // Read "{"
     if (readFrom.findInLine(".").charAt(0) != '{') {
@@ -25,26 +37,52 @@ public class Context implements IContext {
     }
 
     // Extract context string
-    char n = readFrom.findInLine(".").charAt(0);
-    while (n != '}') {
-      contString.append(n);
-      n = readFrom.findInLine(".").charAt(0);
+    while (!readFrom.hasNext("}")) {
+      preconds.add(new Condition(readFrom));
+      progs.add(null);
+      postconds.add(new Condition(readFrom));
+      if (readFrom.hasNext("}")) {
+        break;
+      }
+      else if(readFrom.findInLine(".").charAt(0) != ',') {
+        throw new Exception("context malformated");
+      }
     }
 
-    this.context = contString.toString();
   }
 
-  public String toString() {
-    return "{" + this.context + "}";
-  }
+//  public String toString() {
+//    return "{" + this.context + "}";
+//  }
 
   public boolean inContext(ICondition prec, IProgram prog, ICondition postc) {
-    // TODO Implement later
-    return true;
+    for (int i = 0; i < preconds.size(); ++i) {
+      if (preconds.get(i).equals(prec) && progs.get(i).equals(prog) && postconds.get(i).equals(postc)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public IContext addToContext(ICondition prec, IProgram prog, ICondition postc) {
-    // TODO Implement later
-    return this;
+    Context newCon = new Context(this);
+    newCon.preconds.add(prec);
+    newCon.progs.add(prog);
+    newCon.postconds.add(postc);
+
+    return newCon;
   }
+
+  public ArrayList<ICondition> getPreconds() {
+    return preconds;
+  }
+
+  public ArrayList<IProgram> getProgs() {
+    return progs;
+  }
+
+  public ArrayList<ICondition> getPostconds() {
+    return postconds;
+  }
+
 }
